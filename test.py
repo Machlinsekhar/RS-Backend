187056fe49a0c247d91161002f5a63a4d8e07aa8
Machlin <machlinsekhar@gmail.com>
2023-10-29 19:06:20 +0530

    first push

---
+++ test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+
